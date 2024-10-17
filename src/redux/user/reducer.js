09dc4fab01a1af0d8a67afecd5fb46bcdfb5c7ba
Victor Nunes <nunes.victoramaral@gmail.com>
2024-10-16 22:44:11 -0300

    initial setup of redux

---
+++ src/redux/user/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    currentUser: null,
+};
+
+const userReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default userReducer;

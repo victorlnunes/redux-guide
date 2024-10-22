41fe3a422ad69f2625e37f9d8fe283eb8512e6a3
Victor Nunes <nunes.victoramaral@gmail.com>
2024-10-21 21:35:03 -0300

    changes on store.js to use redux toolkit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
-import { createStore, applyMiddleware } from "redux";
-import logger from "redux-logger"
+import { configureStore, applyMiddleware } from "@reduxjs/toolkit";
+import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(logger),
+});
 
 export default store;

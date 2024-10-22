a2bad8c97d0ef0a5ac76bbaaf9debd2e838646f3
Victor Nunes <nunes.victoramaral@gmail.com>
2024-10-21 22:01:36 -0300

    removes unnecessary import

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore, applyMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
